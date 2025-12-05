61d54aaaa4be5d2ced59e852df7b71abfafc0f31
marfellor <marcfelixlorca@gmail.com>
2025-12-05 13:25:29 +0100

    codigo de la rama

--- HolaGitIntelliJ/src/Main.java
+++ HolaGitIntelliJ/src/Main.java
@@ -2,4 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("¡Hola desde IntelliJ y Git!");
     }
+    public static void mostrarSaludo(String nombre) {
+        System.out.println("¡Hola, " + nombre + "! Bienvenido al proyecto.");
+    }
 }
